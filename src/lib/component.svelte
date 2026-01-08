090bc5ed3a9d08e2197fda6b2b504806e3481193
Moses Miller <moses@memiller.net>
2026-01-07 16:59:58 -0800

    Simpler example

--- src/lib/component.svelte
+++ src/lib/component.svelte
@@ -1,9 +1,3 @@
 <script lang="ts">
     let { data } = $props();
 </script>
-
-<ol>
-    {#each data as item (item) }
-        <li>Some data: {item}</li>
-    {/each}
-</ol>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,11 +3,6 @@
     import { getData } from "./data.remote";
 </script>
 
-<h1>Welcome to SvelteKit</h1>
-<p>
-    Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the
-    documentation
-</p>
 <svelte:boundary>
     <Component data={await getData()} />
 </svelte:boundary>
